f2d312d0b2fb71b0d16d7c26386a576c0a282d60
aaschwab <aries7020@gmail.com>
2024-03-15 09:20:17 -0500

    added a function for AI search

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function generateForm(event) {
+  event.preventDefault();
+  let dogBreedElement = document.querySelector("#dog-breed");
+  dogBreedElement.innerHTML = "Norwegain Elkhound is your breed";
+}
 
+//
+let dogFormElement = document.querySelector("#input-form");
+dogFormElement.addEventListener("submit", generateForm);
