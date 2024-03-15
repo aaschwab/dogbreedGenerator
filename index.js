cbac3c5cfb2088b869bcc4c15c27f56e4b2abc0d
aaschwab <aries7020@gmail.com>
2024-03-15 10:06:11 -0500

    added AI & axios to dog breed generator

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+function displayBreed(response) {
+  new Typewriter("#dog-breed", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+  });
+}
+
 function generateForm(event) {
   event.preventDefault();
 
-  new Typewriter("#dog-breed", {
-    strings: ["Norwegain Elkhound is your breed"],
-    autoStart: true,
-    delay: 3,
-    cursor: "",
-    deleteSpeed: false,
-  });
+  let apiKey = "44ebfbe3f90e8afo31a00943c4996bft";
+  let prompt =
+    "you are given the lifestyle from the user, please select 1 dog breed that matches the lifestyle that the user gives, and give 3 short facts about the dog breed that you chose.";
+  let context =
+    "Please be polite, give an answer that would be most compatible with the lifestyle that is given by the user. Use the words that are given to you in your answer please.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayBreed);
 }
 
 //
